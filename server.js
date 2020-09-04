59ba2b0addc370e97278a292487ea177f0a646c6
Crystal <crystalwatkins428@gmail.com>
2020-09-03 20:06:02 -0400

    updated cors

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const email = require("./routes/email");
 const path = require("path");
 const app = express ();
 require("dotenv").config();
+const cors = require("cors");
+
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+})
 
 app.use("/api/email", email);
 
